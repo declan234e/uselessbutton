bc3b090e71e33cb6526a45d54327becb661b24e3
declan234e <44881262+declan234e@users.noreply.github.com>
2019-06-19 20:29:11 +0100

    Update idle-breakout-save-maker.py

--- uselessbutton2_exported/idle-breakout-save-maker.py
+++ uselessbutton2_exported/idle-breakout-save-maker.py
@@ -1,32 +1,11 @@
-import base64
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-print('gamer234e.ga')
+# enable debugging
+import cgitb
+cgitb.enable()
 
-print('idle  breakout import hack')
+print("Content-Type: text/plain;charset=utf-8")
+print()
 
-print("What level do you want to be on?")
-level = input()
-
-print("What amount of money would you like to have")
-money = input()
-
-print("How much gold do you want")
-gold = input()
-
-print("How many Black Boxes?")
-bb = input()
-
-print("How many skillpoints")
-sp = input()
-
-s = f"{level},{money},{gold},2,0,0,0,0,0,0,0,1,1,0,43595.78,999999,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{bb},0,0,0,1,{sp},1,0,0"
-b = s.encode("UTF-8")
-e = base64.b64encode(b)
-print("Encoding....")
-print("Your result is....")
-print(e)
-print("\nCopy whats INSIDE the quotes\n")
-end = 1
-while end == 1:
-    print("Once copied you may exit the console")
-    input()
+print("Hello World!")
